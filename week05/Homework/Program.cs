25f803dc3e548fd2353d188d424a4ab94ce7a5de
Miracle-Lawrence <youngmiracle01@gmail.com>
2025-04-05 10:53:40 +0100

    Week 5 prep completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment Ass = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(Ass.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment Math = new MathAssignment("Roberto Rodriguez","Fractions","7.3","8-19");
+        Console.WriteLine(Math.GetSummary());
+        Console.WriteLine(Math.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Mary Water","European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
