a22f568592f54c3d5bfa8054bbf1cccecc6fd3d0
Miracle-Lawrence <youngmiracle01@gmail.com>
2025-04-12 23:04:29 +0100

    Week 6 project completed

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,13 @@
+//To exceed requirements, i did the following;
+// Added a "LargeGoal" class to track progress towards a large goal like running a marathon, earning points as progress is made.
+// Added a "NegativeGoal" class that deducts points for bad habits or undesirable actions, allowing for penalty-based goal tracking.
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager goalManager = new GoalManager();
+        goalManager.Start();
     }
 }
